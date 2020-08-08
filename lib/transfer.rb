1bfbb24445fbcc08bbdac20631965b84c1b34ccb
Pablo Menz <p.menz.r@gmail.com>
2020-08-08 22:34:12 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -25,7 +25,12 @@ class Transfer
   end 
   
   def reverse_transfer 
-    
+  if valid? && receiver.balance > amount && self.status == "complete"
+      receiver.balance -= amount
+      sender.balance += amount
+      self.status = "reversed"
+    else
+      reject_transfer  
   end
   
   def reject_transjer
