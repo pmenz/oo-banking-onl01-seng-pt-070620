5108755459fc4ff5d752446b4bb9860e649dd2d3
Pablo Menz <p.menz.r@gmail.com>
2020-08-08 22:28:59 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,6 +1,7 @@
 class Transfer
   
-  attr_accessor:sender, :receiver, :amount, :status
+  attr_accessor:sender, :receiver, :amount
+  attr_accessor:status
   
   def initialize(sender, receiver, amount)
     @sender = sender 
